75ea2c1f176f8a667b3b4394a272b296b1b06ae3
harisasif07 <harisasif555@gmail.com>
2025-07-07 18:58:43 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,11 @@
+console.log(3+3);
+console.log("Haris")
+
+let age = 23
+const name = "haris"
+
+
+console.log(typeof age )
+//console.log( typeof age )
+//output will be number because this shows the type of data 
+
