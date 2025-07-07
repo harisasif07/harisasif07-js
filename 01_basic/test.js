37a8e572caaf6af7ecdb76aaad644a71a3bda519
harisasif07 <harisasif555@gmail.com>
2025-07-07 18:16:16 +0000

    variables

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,5 @@
 console.log("Haris")
+
+//view-> terminal-> node File_Folder / File_name 
+
+
---
+++ 01_basic/variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail =  "harisasif555@gmail.com"
+var accountpassword = "1234"
+let accountstate;
+
+//variables are const, let, var etc 
+//string, Boolean etc is also part of of variable
+// const values are fixed 
+
+//console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountstate]);
+
+/*
+Not use VAR variable because of issue in block and functional scope 
+if we let any object and odesnot define any value to it so it will be considered as UNDEFINED 
+*/
+
