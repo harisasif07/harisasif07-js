bef581c20b3194e5c9a8b6bfdbb287acb7a3257b
harisasif07 <harisasif555@gmail.com>
2025-07-08 17:29:15 +0000

    edited

--- 01_basic/datatypes.js
+++ 01_basic/datatypes.js
@@ -1,11 +1,65 @@
-console.log(3+3);
-console.log("Haris")
+//console.log(3+3);
+//console.log("Haris")
 
 let age = 23
 const name = "haris"
 
-
-console.log(typeof age )
+//console.log(typeof age)
 //console.log( typeof age )
 //output will be number because this shows the type of data 
 
+let value = 33 
+let negvalue = -value
+//console.log(negvalue);
+
+let isloggedIn = 1
+//console.log(isloggedIn)
+let BooleanisloggedIn = Boolean(isloggedIn)
+//console.log(BooleanisloggedIn)
+
+let isloggedOff = 0
+//console.log(isloggedOff)
+let BooleanisloggedOff = Boolean(isloggedOff)
+//console.log(BooleanisloggedOff)
+//console.log(typeof BooleanisloggedIn);
+
+let str1 = "hello"
+let str2 = " Haris"
+let str3 = str1 + str2 
+//console.log(str3);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2/2);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
+
+/* if we add number with string the output will be number plus string
+   if we add string with number the output will be same 
+   if we add 2 number then one string then answer will be in "122"
+   */
+
+
+// let x = 3 
+// x++
+//console.log(x);
+
+// let y = 3 
+// ++y
+//console.log(y);
+
+// let x = 3 
+// let y= x++
+// console.log(x);
+// console.log(y);
+
+// let x = 3 
+// let y= ++x
+// console.log(x);
+// console.log(y);
